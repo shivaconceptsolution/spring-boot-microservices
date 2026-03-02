edc955d29f3edea972e5cbbf4bdf3dcac0b586e0
shivaconceptsolution <shivaconceptsoltion@gmail.com>
2026-03-02 14:02:00 +0530

    Customer services added by developer1

---
+++ customerservice/src/main/java/com/customerservice/CustomerController.java
@@ -0,0 +1,20 @@
+package com.customerservice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+	@GetMapping("/customer")
+    public String secured() {
+        return "This is First Customer Service";
+    }
+}
